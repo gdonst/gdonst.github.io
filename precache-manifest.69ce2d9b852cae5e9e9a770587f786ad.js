0500c21c9911ac7822699155c252e89f15590397
Grant <gdons849@mtroyal.ca>
2020-02-24 23:21:18 -0700

    React Movie Website
    This is a movie information website built with React

---
+++ precache-manifest.69ce2d9b852cae5e9e9a770587f786ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ecd90f186a18f0a962f48def4afa3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c791d16b29b43214bae",
+    "url": "/static/css/main.07dc5408.chunk.css"
+  },
+  {
+    "revision": "42bd515f68822363a3bc",
+    "url": "/static/js/2.47129445.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.47129445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c791d16b29b43214bae",
+    "url": "/static/js/main.a9b1e8af.chunk.js"
+  },
+  {
+    "revision": "b4408b566a0d270befb6",
+    "url": "/static/js/runtime-main.b96bc676.js"
+  },
+  {
+    "revision": "e9a1d15840767a56841efe87f3d5997e",
+    "url": "/static/media/peter-lewicki-Wfh650C1OHU-unsplash.e9a1d158.jpg"
+  }
+]);
